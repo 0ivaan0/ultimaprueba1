c239e7c75ec2d4fffeededf15173a01d728dc929
0ivaan0 <ivanperezminguez@gmail.com>
2024-04-16 10:01:32 +0200

    Edit rama 1

--- src/Main.java
+++ src/Main.java
@@ -20,5 +20,7 @@ public class Main {
             System.out.println("Nombre: " + deporte.getNombre() + ", Jugadores: " + deporte.getCantidadJugadores());
 
             System.out.println("hoal");
+
+            //Estoy editando la rama1
         }
     }
